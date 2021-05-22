b77cfe39a27b063e70dd7c6675fb7ba7ee990f7d
Ursin Schleiss <uschleiss@online.gibz.ch>
2021-05-22 16:02:22 +0200

    backend push

--- frontend/components/ducks.ts
+++ frontend/components/ducks.ts
@@ -1,8 +1,20 @@
+const IP: string = "192.168.0.220";
+
 export const postQuestions = (numberOfQuestions: number, difficulties?: Array<string>, categories?: Array<string>) => {
-    return fetch('http://172.24.112.1:3000/questions', {
+    return fetch(`http://${IP}:3000/questions`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({numberOfQuestions: numberOfQuestions})
+        }
+    ).then((response) => {
+        return response.json();
+    });
+}
+
+export const getCategories = () => {
+    return fetch(`http://${IP}:3000/categories`, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'},
         }
     ).then((response) => {
         return response.json();
